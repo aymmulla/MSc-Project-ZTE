f374dc6170363c7cd5edc600b31086141c006748
shookethman <aymanymulla@gmail.com>
2026-06-28 18:24:42 +0100

    Tracking and adjusting gapsize

--- aymansigmri.py
+++ aymansigmri.py
@@ -3,6 +3,7 @@ import sigpy as sp
 import pandas as pd
 import sigpy.plot as pl
 import numpy as np
+import jax as jx
 
 
 #First 4 funtions used to get toy ZTE style data
@@ -236,13 +237,24 @@ def hankel_H_averaged(data_matrix, n_coils, Nx, Ny, w=3):
         kspace.append(recon.T)
     return np.array(kspace)
 
+def sig_val_thresholding_fast(data, zero_thresh):
+    U, Sigma, Vh = np.linalg.svd(data, full_matrices=False)
+    thresh = Sigma.max() * zero_thresh
+    S_reduced = np.where(Sigma > thresh, Sigma, 0)
+    return U, S_reduced, Vh
+
+def sig_val_thresholding_jax(data, zero_thresh):
+    U, Sigma, Vh = jx.numpy.linalg.svd(data, full_matrices=False)
+    thresh = Sigma.max() * zero_thresh
+    S_reduced = np.where(Sigma > thresh, Sigma, 0)
+    return U, S_reduced, Vh
 
 def sig_val_thresholding(data, zero_thresh):
     U, Sigma, Vh = np.linalg.svd(data, full_matrices=False)
     max_val = np.max(Sigma)
     thresh = max_val*zero_thresh
     arr = []
-    for i in range(len(Sigma)):
+    for i in range(len(Sigma)): ##Sigma[Sigma < thresh] = 0
         if Sigma[i] > thresh:
             arr.append(Sigma[i])
         else: arr.append(0)
@@ -334,3 +346,96 @@ def zero_padding(cart_kspace,resize_x, resize_y):
     enlarged_image_grid = sp.resize(image_grid, [n_coils,resize_x, resize_y])
     enlarged_cartesian_kspace = sp.fft(enlarged_image_grid, axes=(-2, -1))
     return enlarged_cartesian_kspace
+
+
+def plot_mask(isolated_kspace, mask, zte_radial_coords, zte_radial_kspace, innersidelen, sidelencart, sidelenrad):
+    
+    all_coords = zte_radial_coords.reshape(-1, 2)
+
+    fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(30, 8))
+    
+    im = ax[0].imshow(np.abs(isolated_kspace[0]), cmap='viridis', origin='lower')
+    fig.coPbar(im, ax=ax[0], label='kspace value')
+    ax[0].set_title('Cartesian')
+    ax[0].set_xlabel('kx')
+    ax[0].set_ylabel('ky')
+    ax[0].set_xlim(innersidelen/2 - sidelencart,innersidelen/2 + sidelencart)
+    ax[0].set_ylim(innersidelen/2 - sidelencart,innersidelen/2 + sidelencart)
+    ax[0].xaxis.set_minor_locator(plt.MultipleLocator(1, offset=0.5))
+    ax[0].yaxis.set_minor_locator(plt.MultipleLocator(1, offset=0.5))
+    ax[0].grid(visible=True, which='minor', linewidth=1)
+    ax[0].xaxis.set_major_locator(plt.MultipleLocator(1))
+    ax[0].yaxis.set_major_locator(plt.MultipleLocator(1))
+
+
+    yy, xx = np.mgrid[0:mask.shape[0], 0:mask.shape[0]]
+    im1 = ax[1].scatter(xx.ravel(), yy.ravel(), c=mask.ravel().astype(int), cmap='viridis', s=300, marker='s', zorder=3)
+    fig.colorbar(im1, ax=ax[1], label='mask')
+    ax[1].set_title('Centre mask')
+    ax[1].set_xlabel('x')
+    ax[1].set_ylabel('y')
+    ax[1].axis('equal')
+    ax[1].set_xlim(innersidelen/2 - sidelencart,innersidelen/2 + sidelencart)
+    ax[1].set_ylim(innersidelen/2 - sidelencart,innersidelen/2 + sidelencart)
+    ax[1].xaxis.set_minor_locator(plt.MultipleLocator(1, offset=0.5))
+    ax[1].yaxis.set_minor_locator(plt.MultipleLocator(1, offset=0.5))
+    ax[1].grid(visible=True, which='minor', linewidth=1)
+    ax[1].xaxis.set_major_locator(plt.MultipleLocator(1))
+    ax[1].yaxis.set_major_locator(plt.MultipleLocator(1))
+
+    im2 = ax[2].scatter(all_coords[:, 0],all_coords[:, 1], c=np.abs(zte_radial_kspace[1]), cmap='viridis')
+    fig.colorbar(im2, ax=ax[2], label='kspace value')
+    ax[2].set_title('Radial')
+    ax[2].set_xlabel('kx')
+    ax[2].set_ylabel('ky')
+    ax[2].axis('equal')
+    ax[2].xaxis.set_minor_locator(plt.MultipleLocator(1, offset=0.5))
+    ax[2].yaxis.set_minor_locator(plt.MultipleLocator(1, offset=0.5))
+    ax[2].grid(visible=True, which='minor', linewidth=1)
+    ax[2].xaxis.set_major_locator(plt.MultipleLocator(1))
+    ax[2].yaxis.set_major_locator(plt.MultipleLocator(1))
+    ax[2].set_xlim(0 - (sidelenrad//2), 0 + (sidelenrad//2))
+    ax[2].set_ylim(0 - sidelenrad//2, 0 + (sidelenrad//2))
+
+    plt.show()
+
+def inner_portion(enlarged_kspace, inner_sidelen):
+    """Take an inner square of kspace to speed up the hankel loop
+
+        Args:
+            enlarged_kspace: Zero padded kspace in cartesian coordinates (n_coils, nx, ny)
+            inner_sidelen: Side legnth of the inner square, divided by 2 to find from central point outward
+
+        Returns:
+            isolated_kspace: Inner kspace array
+            isolation_mask: Mask of the inner region to use when 'jigsawing' the transformed array back in
+            Start: xy index of the enlarged array that the inner region starts at
+            End: xy index of the enlarged array that the inner region ends at
+    """
+    cx, cy = enlarged_kspace.shape[1] // 2, enlarged_kspace.shape[2] // 2
+    N = inner_sidelen/2
+    start, end = int(cx-N), int(cx+N)
+    isolation_mask = np.ones(enlarged_kspace.shape[1:], dtype=int)
+    isolation_mask[start:end, start:end] = 0
+    isolated_kspace = enlarged_kspace[:, start:end, start:end]
+    
+    return(isolated_kspace, isolation_mask, start, end)
+
+
+def jigsaw(output_kspace, isolation_mask, start, end, enlarged_kspace):
+    ### NOTE: There is a better way to do this without using start and end
+    """Put the transormed region of kspace back into the zero padded region
+        Args:
+            output_kspace: Transformed SVD thresh kspace region
+            isolation_mask: Mask of the inner region; typically output of asm.inner_portion
+            Start: xy index of the enlarged array that the inner region starts at; typically output of asm.inner_portion 
+            End: xy index of the enlarged array that the inner region ends at; typically output of asm.inner_portion
+            enlarged_kspace: Zero padded kspace in cartesian coordinates (n_coils, nx, ny)
+
+        Returns:
+            recombined: Zero padded kspace with transfomed inner region 'jigsawed' back in
+    """
+    processed_isolated_kspace = output_kspace.copy()
+    recombined = enlarged_kspace * isolation_mask ### NOTE: Need to improve this
+    recombined[:, start:end, start:end] = processed_isolated_kspace
+    return(recombined)
